5b206620e31b737dd37b7d8abf05ec38593bc669
Balazs Kis <zsomix.balu@gmail.com>
2025-02-02 12:23:27 +0000

    Initial Commit

---
+++ iosAppDevelopmentUni/iosAppDevelopmentUni/iosAppDevelopmentUniApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppDevelopmentUniApp.swift
+//  iosAppDevelopmentUni
+//
+//  Created by Herényi Orsolya on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct iosAppDevelopmentUniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
